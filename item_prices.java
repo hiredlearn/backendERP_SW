d2d58eeddcca3614dbd89c468df20ec80f3c6160
Robin Yancey <reyancey@ucdavis.edu>
2021-07-25 18:24:08 -0700

    Update item_prices.java

--- item_prices.java
+++ item_prices.java
@@ -1,5 +1,6 @@
-// write a function to return the prices for all I inventory items in SaleItem[I]
-// the function should take an input of ASale.items, where
+// write a function to return the prices for all inventory items in an order (SaleItems)
+// for each price I store it in the variable SaleItems[I]
+// the function should take an input of J ASale.items, where
 // ASale.items[J] is the price (if any)
 // ASale.items[J].product_type is item type (eg. 'configurable or 'simple')
 // ASale.items[J].parent_item_id is the parent item id (if any)
