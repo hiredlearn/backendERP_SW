b6976eb5547dd6199b46eadb3c9c71f928f85588
Robin Yancey <reyancey@ucdavis.edu>
2021-07-25 17:22:33 -0700

    Create item_prices.java

---
+++ item_prices.java
@@ -0,0 +1,7 @@
+// write a function to return the prices for all I inventory items in SaleItem[I]
+// the function should take an input of ASale.items, where
+// ASale.items[J] is the price (if any)
+// ASale.items[J].product_type is item type (eg. 'configurable or 'simple')
+// ASale.items[J].parent_item_id is the parent item id (if any)
+// ASale.items[J].item_id is the item id 
+// ASale.items.sku is the indicator that the configurable comes AFTER (not before)
